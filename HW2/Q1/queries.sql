c7960b409124aa95b970315cd21d87c4861a82bb
baiweilu111 <168377047+baiweilu111@users.noreply.github.com>
2024-05-06 20:30:02 -0700

    Delete HW2 directory

--- HW2/Q1/queries.sql
+++
@@ -1,8 +0,0 @@
-SELECT FLOOR(AvgRating) as RatingBin, COUNT(*) as MovieCount
-FROM (
-    SELECT Movie_FK, AVG(Rating) as AvgRating, COUNT(Rating) as TotalVotes
-    FROM Ratings
-    GROUP BY Movie_FK
-    HAVING COUNT(Rating) >= 1000
-) as FilteredMovies
-GROUP BY FLOOR(AvgRating);
